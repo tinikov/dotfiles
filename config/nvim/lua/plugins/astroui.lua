1a0db6d3807e0161ff692022a1bdbd2276d651ee
tinikov <i@tinikov.com>
2024-12-26 18:31:29 +0900

    change to light mode

--- config/nvim/lua/plugins/astroui.lua
+++ config/nvim/lua/plugins/astroui.lua
@@ -12,8 +12,8 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "astrodark",
-    colorscheme = "catppuccin-macchiato",
-    -- colorscheme = "tokyonight",
+    -- colorscheme = "catppuccin-macchiato",
+    colorscheme = "catppuccin-latte",
 
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
