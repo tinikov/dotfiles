79ebf93ba1b219f41959513f4871273a11e4fa8c
tinikov <i@tinikov.com>
2025-03-21 16:50:49 +0900

    brewfile changed;nvim&fish greetings changed

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,22 +1,25 @@
 function fish_greeting
-    set_color -o brblue
-    echo "╭────────────────────────────────────────╮"
-    echo "│                                        │"
-    echo "│           "(set_color bryellow)"THE FUTURE IS NOW"(set_color brblue)"            │"
-    echo "│                                        │"
-    echo "╰────────────────────────────────────────╯"
+    # set_color -o brblue
+    # echo "╭────────────────────────────────────────╮"
+    # echo "│                                        │"
+    # echo "│           "(set_color bryellow)"THE FUTURE IS NOW"(set_color brblue)"            │"
+    # echo "│                                        │"
+    # echo "╰────────────────────────────────────────╯"
+    # set_color -o
+    # echo "       $(date '+%Y年%m月%d日 %A %H:%M')"
     
-    set_color green
-    set_color -o
-    echo "       $(date '+%Y年%m月%d日 %A %H:%M')"
+    set_color -io green
+    echo "$(date '+%Y年%m月%d日 %A %H:%M')"
     set_color normal
-    
-    echo
-    set_color -o
-    echo " aliases"
+    set_color -i bryellow
+    echo "  \"THE FUTURE IS NOW\""
     set_color normal
-    echo " ‣ capslock <-> esc:  "(set_color cyan)"vmode nmode"(set_color normal)
-    echo " ‣ brew maintenance:  "(set_color cyan)"brup brclean"(set_color normal)
-    echo " ‣ proxy:  "(set_color cyan)"proxy-on proxy-off proxy-stat"(set_color normal)
+    set_color -io
+    echo " Aliases"
+    set_color normal
+    echo " ‣ capslock <-> esc:  "(set_color -io cyan)"vmode nmode"(set_color normal)
+    echo " ‣ brew maintenance:  "(set_color -io cyan)"brup brclean"(set_color normal)
+    echo " ‣ proxy:  "(set_color -io cyan)"proxy-on proxy-off proxy-stat"(set_color normal)
+    set_color normal
     echo
 end
